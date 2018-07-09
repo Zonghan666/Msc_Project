db8035ca6c06fc1d36275709bbdc66969c1f3a9b
Zonghan <zhang.zonghan13@outlook.com>
2018-07-09 15:31:01 +0100

    test

--- Yolov3/files_helper.py
+++ Yolov3/files_helper.py
@@ -1,5 +1,6 @@
 import xml.dom.minidom
 import numpy as np
+import parameter
 
 
 def annotation_reader(file_path):
@@ -16,12 +17,12 @@ def annotation_reader(file_path):
     dom = xml.dom.minidom.parse(file_path)
     root = dom.documentElement
 
-    #load the size of the image
+    # load the size of the image
     size_xml = root.getElementsByTagName('size')[0]
     img_width = float(size_xml.getElementsByTagName('width')[0].firstChild.data)
     img_height = float(size_xml.getElementsByTagName('height')[0].firstChild.data)
 
-    #iterate the object
+    # iterate the object
     objects = root.getElementsByTagName('object')
 
     result = []
@@ -39,25 +40,21 @@ def annotation_reader(file_path):
         center_x = xmin + obj_width / 2
         center_y = ymin + obj_height / 2
 
-        #normalize
+        # normalize
         center_x = center_x / img_width
         center_y = center_y / img_height
         obj_width = obj_width / img_width
         obj_height = obj_height / img_height
 
-        #convert class to int
-        if object_name == 'nipple':
-            cls = 0
-            
-        
+        # convert class to int
+        assert (object_name in parameter._CLASSES)
+        cls = parameter._CLASSES[object_name]
 
         result.append((cls, center_x, center_y, obj_width, obj_height))
 
-
     result = np.array(result)
 
-    #expand the dims
+    # expand the dims
     result = np.expand_dims(result, 0)
-
 
     return result
