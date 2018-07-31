3b569603b7ca6c0f190b25b6a9fb27c3d75d7fe2
Zonghan <zhang.zonghan13@outlook.com>
2018-07-31 17:43:44 +0100

    add function to suppress background

--- preprocess/pectoral_muscle_remove.py
+++ preprocess/pectoral_muscle_remove.py
@@ -3,6 +3,12 @@ import numpy as np
 import skfuzzy as fuzz
 
 def remove_label(img_path):
+    """
+    function to remove label and other noise and reserve the brest part
+    :param img_path: path of the processed image
+    :return: result n-array cv2 format
+    """
+
     img = cv2.imread(img_path, 0)
     ret, thresh = cv2.threshold(img, 15, 255, cv2.THRESH_BINARY)
     # smooth = cv2.GaussianBlur(thresh,(5,5), 0)
@@ -11,6 +17,7 @@ def remove_label(img_path):
     idx = np.argmax(contours_area)
 
     mask = np.zeros_like(img)
+    # we just keep the Contours with the largest area, which is normally the brest part
     mask = cv2.drawContours(mask, [contours[idx]], -1, 255, -1)
     mask = (mask > 0)
     result = img * mask
@@ -19,14 +26,22 @@ def remove_label(img_path):
 
 
 def remove_pectoral_muscle(img_path):
+    """
+    function to remove the pectoral muscle
+    :param img_path: path of the image
+    :return: result_img n-array cv2 format
+    """
+
     img = remove_label(img_path)
     reshape = img.reshape([img.shape[0] * img.shape[1], 1]) / 255
     data = reshape.T
+    # apply fuzzy-c-means algorithm
     cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(data, 3, 2, error=0.005, maxiter=1000, init=None)
     a = np.argmax(u, axis=0)
     result = a.reshape(img.shape[0], img.shape[1])
 
     cnt_cluster = []
+    # find the pectoral muscle cluster
     for i in range(3):
         result_muscle = (result == i)
         mask = np.zeros_like(img)
@@ -39,6 +54,7 @@ def remove_pectoral_muscle(img_path):
     contours_area = np.array([cv2.contourArea(contour) for contour in cnt_cluster])
     idx = np.argmin(contours_area)
     cnt = cnt_cluster[idx]
+    # approximate the raw 'bad' shape
     epsilon = 0.03 * cv2.arcLength(cnt, True)
     approx = cv2.approxPolyDP(cnt, epsilon, True)
     mask = np.zeros_like(img)
@@ -48,3 +64,36 @@ def remove_pectoral_muscle(img_path):
     result_img = img * mask
 
     return result_img
+
+
+def cut_background(img, ratio=0.8):
+    """
+    function to cut the useless background (rectangle)
+    :param img: n-array cv2 format
+    :param ratio: ratio of background that need to be cut
+    :return: img n-array cv2 format
+    """
+    # first we sum over the columns, background should be the columns of sum 0
+    img_sum = np.sum(img, axis=0)
+
+    # next we need to check whether the background is on the right of left part of the images
+    # we need to standardise the image so that the background is on the right hand side
+    if img_sum[0] == 0:
+        img_type = 'right'
+    elif img_sum[-1] == 0:
+        img_type = 'left'
+
+    if img_type == 'right':
+        img = img[:, ::-1]
+        img_sum = img_sum[::-1]
+
+    # use np.argmin() to find the border of the background
+    start = np.argmin(img_sum)
+    end = img_sum.shape[0] - 1
+    reserved_pixels = int((end - start) * (1 - ratio))
+    img = img[:, :start + reserved_pixels]
+
+    if img_type == 'right':
+        img = img[:, ::-1]
+
+    return img
